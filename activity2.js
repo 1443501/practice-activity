616547f0658f9c005c6ea1c39909c15aef8e64ed
1443501 <106714590+1443501@users.noreply.github.com>
2023-03-14 20:39:45 -0400

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,9 @@
+function back(){
+window.location="activity_1.html";
+}
+
+function getScore(){
+score=localStorage.getItem("score");
+document.getElementById("output").innerHTML="score: "+score;
+
+}
